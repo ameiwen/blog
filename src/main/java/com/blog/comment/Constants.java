54657f794eeb531ff99c5d833a958e280db4c00f
wupei1994 <969631699@qq.com>
2018-01-31 13:58:30 +0800

    ~

--- src/main/java/com/blog/comment/Constants.java
+++ src/main/java/com/blog/comment/Constants.java
@@ -7,8 +7,8 @@ public class Constants {
     public static final String IP_SEARCH_APPCODE = "715dbc14d8544e78907c0ea22658ac3e";
 
     public static final String END_POINT = "oss-cn-beijing.aliyuncs.com";
-    public static final String ACCESS_KEY_ID = "LTAIbUwIUr8etm3j";
-    public static final String ACCESS_KEY_SECRET = "6qjVq9gbihH84ANb4puHbXznjTd4xt";
+    public static final String ACCESS_KEY_ID = "";
+    public static final String ACCESS_KEY_SECRET = "";
 
     public static final String BUCKET_NAME = "yangxs";
     public static final String URL="http://yangxs.oss-cn-beijing.aliyuncs.com/";
